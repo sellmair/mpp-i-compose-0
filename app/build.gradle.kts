7a2d37e9a3e986b26a7d5dab3d3bb2f48d633717
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-09-17 16:34:10 +0200

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,89 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-multiplatform")
+}
+
+android {
+    compileSdkVersion(30)
+    sourceSets {
+        named("main") {
+            this.java.srcDir(file("src/androidMain/kotlin"))
+        }
+    }
+}
+
+kotlin {
+    android()
+    jvm("desktop")
+
+    sourceSets {
+        val jvmMain by register("jvmMain")
+        val jvmTest by register("jvmTest")
+
+        getByName("desktopMain") {
+            dependsOn(jvmMain)
+        }
+        getByName("androidMain") {
+            dependsOn(jvmMain)
+        }
+        getByName("androidTest") {
+            dependsOn(jvmTest)
+        }
+        getByName("androidAndroidTest") {
+            dependsOn(jvmTest)
+        }
+    }
+}
+
+
+/*plugins {
+    id 'com.android.application'
+    id 'kotlin-android'
+}
+
+android {
+    compileSdkVersion 30
+    buildToolsVersion "30.0.2"
+
+    defaultConfig {
+        applicationId "com.example.mpp_compose_0"
+        minSdkVersion 21
+        targetSdkVersion 30
+        versionCode 1
+        versionName "1.0"
+
+        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            minifyEnabled false
+            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+    compileOptions {
+        sourceCompatibility JavaVersion.VERSION_1_8
+        targetCompatibility JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = '1.8'
+    }
+
+}
+
+dependencies {
+
+    implementation "org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version"
+    implementation 'androidx.core:core-ktx:1.3.1'
+    implementation 'androidx.appcompat:appcompat:1.2.0'
+    implementation 'com.google.android.material:material:1.2.0'
+    testImplementation 'junit:junit:4.+'
+    androidTestImplementation 'androidx.test.ext:junit:1.1.1'
+    androidTestImplementation 'androidx.test.espresso:espresso-core:3.2.0'
+}
+
+ */
---
+++ app/src/desktopMain/kotlin/DesktopMainConsumer.kt
@@ -0,0 +1,1 @@
+class DesktopMainConsumer : JvmMainApi, CommonMainApi
